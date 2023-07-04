2622bb60f73edfe56b926a99687140fe84b19e6d
Ricardo Neves <ricardodcmeloneves@gmail.com>
2023-07-04 05:09:47 +0100

    restfull

---
+++ API/restFull/src/main/java/org/kamicaze/domain/repository/ItemsPedido.java
@@ -0,0 +1,7 @@
+package org.kamicaze.domain.repository;
+
+import org.kamicaze.domain.entity.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemsPedido extends JpaRepository<ItemPedido,Integer> {
+}
